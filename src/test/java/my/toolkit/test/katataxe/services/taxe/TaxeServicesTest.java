8c89da8f9385359c43af723ef9067556f9c4ad10
Michel Narouman <mnarouman@ausy.fr>
2017-09-06 08:21:48 +0100

    Le 3eme cas ne passe passe : normal
    On va modifier la méthode qui renvoyé une constante...

--- src/test/java/my/toolkit/test/katataxe/services/taxe/TaxeServicesTest.java
+++ src/test/java/my/toolkit/test/katataxe/services/taxe/TaxeServicesTest.java
@@ -7,9 +7,11 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TaxeServicesTest {
-
+	private TaxeProvider taxeProvider ;
+	
 	@Before
 	public void setUp() throws Exception {
+		taxeProvider = TaxeProvider.getUniqueInstance();
 	}
 
 	@After
@@ -19,7 +21,6 @@ public class TaxeServicesTest {
 	@Test
 	public void arrondiWhen0_99Then1_00() {
 		//given
-		TaxeProvider taxeProvider = TaxeProvider.getUniqueInstance();
 		double input = 0.99;
 		double expected = 1.00;
 		
@@ -30,4 +31,29 @@ public class TaxeServicesTest {
 		assertThat(actual).isEqualTo(expected);
 	}
 
+	@Test
+	public void arrondiWhen1_00Then1_00() {
+		//given
+		double input = 1.00;
+		double expected = 1.00;
+		
+		//when
+		double actual = taxeProvider.round(input);
+		
+		//then
+		assertThat(actual).isEqualTo(expected);
+	}
+
+	@Test
+	public void arrondiWhen1_01Then1_05() {
+		//given
+		double input = 1.01;
+		double expected = 1.05;
+		
+		//when
+		double actual = taxeProvider.round(input);
+		
+		//then
+		assertThat(actual).isEqualTo(expected);
+	}
 }
